d4140cd1815591e8f5bf75e966015dd19ed7deb7
admEug <zmaeul@gmail.com>
2025-08-22 23:57:07 +0300

    Added crash test

--- Blazorcrud.Server.Tests/PersonControllerTests.cs
+++ Blazorcrud.Server.Tests/PersonControllerTests.cs
@@ -24,18 +24,18 @@ namespace Blazorcrud.Server.Tests
         [Fact]
         public void GetPeople_Returns_OkResult_With_PagedResult_Of_Person()
         {
-            // Arrange
-            var name = "Test";
+            // Arrange
+            var name = "Test";
             var page = 1;
             var pagedResult = new PagedResult<Person> { Results = new List<Person> { new Person { PersonId = 1, FirstName = "Test" } } };
             _mockRepo.Setup(repo => repo.GetPeople(name, page))
-                .Returns(pagedResult);
+              .Returns(pagedResult);
 
-            // Act
-            var result = _controller.GetPeople(name, page);
+            // Act
+            var result = _controller.GetPeople(name, page);
 
-            // Assert
-            var okResult = Assert.IsType<OkObjectResult>(result);
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
             var returnValue = Assert.IsType<PagedResult<Person>>(okResult.Value);
             Assert.Single(returnValue.Results);
         }
@@ -43,34 +43,34 @@ namespace Blazorcrud.Server.Tests
         [Fact]
         public async Task GetPerson_Returns_OkResult_With_Person()
         {
-            // Arrange
-            var id = 1;
+            // Arrange
+            var id = 1;
             var person = new Person { PersonId = id, FirstName = "Test" };
             _mockRepo.Setup(repo => repo.GetPerson(id))
-                .ReturnsAsync(person);
+              .ReturnsAsync(person);
 
-            // Act
-            var result = await _controller.GetPerson(id);
+            // Act
+            var result = await _controller.GetPerson(id);
 
-            // Assert
-            var okResult = Assert.IsType<OkObjectResult>(result);
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
             var returnValue = Assert.IsType<Person>(okResult.Value);
-            Assert.Equal(id, returnValue.PersonId);
-        }
+            Assert.Equal(99, returnValue.PersonId); // Modified this line to cause the test to fail.
+        }
 
         [Fact]
         public async Task AddPerson_Returns_OkResult_With_Person()
         {
-            // Arrange
-            var person = new Person { PersonId = 1, FirstName = "Test" };
+            // Arrange
+            var person = new Person { PersonId = 1, FirstName = "Test" };
             _mockRepo.Setup(repo => repo.AddPerson(person))
-                .ReturnsAsync(person);
+              .ReturnsAsync(person);
 
-            // Act
-            var result = await _controller.AddPerson(person);
+            // Act
+            var result = await _controller.AddPerson(person);
 
-            // Assert
-            var okResult = Assert.IsType<OkObjectResult>(result);
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
             var returnValue = Assert.IsType<Person>(okResult.Value);
             Assert.Equal(person.PersonId, returnValue.PersonId);
         }
@@ -78,16 +78,16 @@ namespace Blazorcrud.Server.Tests
         [Fact]
         public async Task UpdatePerson_Returns_OkResult_With_Person()
         {
-            // Arrange
-            var person = new Person { PersonId = 1, FirstName = "Test" };
+            // Arrange
+            var person = new Person { PersonId = 1, FirstName = "Test" };
             _mockRepo.Setup(repo => repo.UpdatePerson(person))
-                .ReturnsAsync(person);
+              .ReturnsAsync(person);
 
-            // Act
-            var result = await _controller.UpdatePerson(person);
+            // Act
+            var result = await _controller.UpdatePerson(person);
 
-            // Assert
-            var okResult = Assert.IsType<OkObjectResult>(result);
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
             var returnValue = Assert.IsType<Person>(okResult.Value);
             Assert.Equal(person.PersonId, returnValue.PersonId);
         }
@@ -95,17 +95,17 @@ namespace Blazorcrud.Server.Tests
         [Fact]
         public async Task DeletePerson_Returns_OkResult_With_Person()
         {
-            // Arrange
-            var id = 1;
+            // Arrange
+            var id = 1;
             var person = new Person { PersonId = id, FirstName = "Test" };
             _mockRepo.Setup(repo => repo.DeletePerson(id))
-                .ReturnsAsync(person);
+              .ReturnsAsync(person);
 
-            // Act
-            var result = await _controller.DeletePerson(id);
+            // Act
+            var result = await _controller.DeletePerson(id);
 
-            // Assert
-            var okResult = Assert.IsType<OkObjectResult>(result);
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
             var returnValue = Assert.IsType<Person>(okResult.Value);
             Assert.Equal(id, returnValue.PersonId);
         }
